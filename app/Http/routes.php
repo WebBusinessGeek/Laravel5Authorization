bd6f980668a0844092961c9cabbca5109cb48a1e
Kevin Washington <kevw12188@gmail.com>
2014-10-13 17:37:02 -0400

    some prep

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,3 +12,99 @@
 */
 
 $router->get('/', 'HomeController@index');
+
+
+$router->resource('monkey', 'MonkeyController');
+
+
+$router->resource('banana', 'BananaController');
+
+
+$router->get('/createMonkeys', function(){
+
+	//create monkeys
+	$monkeysArray = [];
+	$monkeyNameBox = [
+
+		'kevin', 
+		'P.Diddy',
+		'Tito',
+		'Deadmaus',
+		'Professor G.',
+
+
+	];
+	$monkeyIdBox = [
+
+		148492,
+		358439,
+		202349,
+		203495,
+		123898,
+
+
+	];
+	for($i = 0; $i < count($monkeyNameBox); $i++){
+		$monkey = new monkey($monkeyNameBox[$i], $monkeyIdBox[$i]);
+		array_push($monkeysArray, $monkey);
+	}
+
+	//create bananas
+	return Session::put('monkeys', $monkeysArray). Session::put('message', 'monkeys are stored');
+
+	
+
+});
+
+$router->get('/check', function(){
+
+	// if(Session::has('monkeys')){
+	// 	return Session::get('monkeys');
+	// }
+	// else{
+	// 	return 'monkeys not here';
+	// }
+
+	if(Session::has('message')){
+		return Session::get('message');
+	}
+});
+
+
+
+
+
+//create monkey class
+
+class monkey {
+
+	//constructor function
+	public function __construct($name, $id){
+
+		$this->name = $name;
+
+		$this->id = $id;
+	}
+
+	//the monkeys name
+	public $name;
+
+	//the monkeys unique id
+	public $id;
+}
+
+
+
+
+//create banana class
+
+
+class banana {
+
+	//what number banana is it?
+	public $number;
+
+	//who is the owner of the banana?
+	public $monkeyId;
+}
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 5 Authorization Example</title>
+
+    <!-- Bootstrap -->
+    <link href="angular1.3-bootstrap3.2/bootstrap-twit/css/bootstrap.min.css" rel="stylesheet">
+
+
+    
+  </head>
+  <body>
+  	<div class="container">
+   	 <h1>Monkeys & Bananas</h1>
+   	 <a href="/createMonkeys" class="btn btn-primary">Create Monkeys</a>
+   	</div>
+
+    
+  </body>
+</html>
